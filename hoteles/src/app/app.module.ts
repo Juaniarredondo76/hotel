6b4b754004512930a6570c7015e2b80392b691cd
401 <401@academicos.cesde.edu.co>
2022-05-12 11:10:29 -0500

    AGR- se crea nuevo rpoyecto de hotel usando angular

---
+++ hoteles/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './shared/menu/menu.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FormularioreservasComponent } from './formularioreservas/formularioreservas.component';
+import { ReservasComponent } from './reservas/reservas.component';
+import { InfohotelComponent } from './infohotel/infohotel.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    FooterComponent,
+    FormularioreservasComponent,
+    ReservasComponent,
+    InfohotelComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
